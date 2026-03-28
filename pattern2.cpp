d8cf3da1de60d4003065eee78bafb5be2a72017d
Chinmay27-lab <byterogue16x@gmail.com>
2026-03-29 01:32:10 +0530

    Star pattern added

---
+++ pattern2.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+void pattern()
+{
+    for(int i = 0 ; i < 5 ; i ++)
+    {
+        for(int j = 0 ; j < 5 ; j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+int main ()
+{
+    pattern();
+}
